0dc9f1c0328cfafc5ee7740c7193f4c4cc9d8367
Luis Roque <luisfroquez@gmail.com>
2023-06-28 09:07:32 -0400

    correcciones varias

--- pages/products2.tsx
+++ pages/products2.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useGetCatalog } from '../src/hooks/useGetCatalog';
-import { Locale } from '../src/enums/locale';
 
 export default function MyComponent() {
     const { data, isLoading, error } = useGetCatalog();
@@ -9,6 +8,7 @@ export default function MyComponent() {
     return (
         <div>
             {/* Render the API response */}
+            <h1>IWS</h1>
             <p>{isLoading}</p>
             {data && <pre>{JSON.stringify(data, null, 2)} </pre>}
         </div>
